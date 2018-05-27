01629d69196767af7d5c8498235eebaefec5e7bc
Matt Watson <mattdangerw@gmail.com>
2018-05-26 21:09:20 -0700

    update scrub script

--- scrub.py
+++ scrub.py
@@ -1,20 +1,24 @@
+"""
+Simple script to remove hard wraps, leading whitespace and
+"""
+
+from __future__ import print_function
 import sys
 import re
-import textwrap
+import os
 
-print(sys.argv[1])
+inpath = sys.argv[1]
+outpath = os.path.join(os.path.dirname(inpath), 'scrubbed.txt')
+print('Input:', inpath)
+print('Output:', outpath)
 
-with open(sys.argv[1], 'r') as textfile:
-    full_text = textfile.read()
+with open(inpath, 'r') as in, open(outpath, 'w') as b:
+    for line in infile:
+        outfile.write(line)
 
-# Remove line wraps
-full_text = re.sub(r'(\S)\n(\S)', r'\1 \2', full_text)
-# Remove extra spacing
-full_text = re.sub(r'\n\s*\n', '\n\n', full_text)
-# Remove leading whitespace
-full_text = '\n'.join(map(lambda line: line.strip(), full_text.splitlines()))
-
-with open(sys.argv[1], 'w') as textfile:
-    textfile.write(full_text)
-
-sys.exit(0)
+# # Remove line wraps
+# full_text = re.sub(r'(\S)\n(\S)', r'\1 \2', full_text)
+# # Remove extra spacing
+# full_text = re.sub(r'\n\s*\n', '\n\n', full_text)
+# # Remove leading whitespace
+# full_text = '\n'.join(map(lambda line: line.strip(), full_text.splitlines()))
